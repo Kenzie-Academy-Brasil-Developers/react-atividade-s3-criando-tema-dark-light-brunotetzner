6a27602e8077a5e86e8513ca3c0c31fd509b996a
Bruno Tetzner <brunoltetzner@gmail.com>
2021-11-30 16:03:52 -0300

    projeto finalizado

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useCallback, useState } from "react";
+import { AppContainer, MainSection } from "./components/app.style";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./theme";
+import { Button } from "./components/button.style";
+
+function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const getOpositeTheme = useCallback(
+    () => (currentTheme === "light" ? "dark" : "light"),
+    [currentTheme]
+  );
+  return (
+    <div className="App">
+      <ThemeProvider theme={themes[currentTheme]}>
+        <AppContainer>
+          <MainSection>
+            <h1>You are in {currentTheme} mode</h1>
+            <Button onClick={() => setCurrentTheme(getOpositeTheme())}>
+              switch to {getOpositeTheme()} mode
+            </Button>
+          </MainSection>
+        </AppContainer>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
